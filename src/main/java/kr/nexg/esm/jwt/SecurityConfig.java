c3138a1b4eb89fd068e714975775c764378c3c95
joduckyong <dyddl7609@gmail.com>
2024-03-08 14:53:04 +0900

    cros 테스트

--- src/main/java/kr/nexg/esm/jwt/SecurityConfig.java
+++ src/main/java/kr/nexg/esm/jwt/SecurityConfig.java
@@ -30,7 +30,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-//				.cors()    
 				.cors().configurationSource(corsConfigurationSource())    
 				.and().csrf().disable()
 				.httpBasic().disable()
