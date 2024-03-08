a636687bcf889b6ecf276c9f0140fb2dc92b7b83
joduckyong <dyddl7609@gmail.com>
2024-03-08 14:56:07 +0900

    cros 적용

--- src/main/java/kr/nexg/esm/common/WebMvcConfiguration.java
+++ src/main/java/kr/nexg/esm/common/WebMvcConfiguration.java
@@ -27,7 +27,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
       // 모든 uri에 대해 http://220.120.28.20:8087, http://220.120.28.20:8811 도메인은 접근을 허용한다.
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("*");
     }
 
 	@Override
